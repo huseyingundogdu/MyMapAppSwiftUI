5d305d6de43ca07851ffc59abf3dafeedb4eddf9
Huseyin <benhuseyingundogdu@gmail.com>
2024-10-02 15:09:45 +0200

    Initial Commit

---
+++ MyMapAppSwiftUI/MyMapAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapAppSwiftUIApp.swift
+//  MyMapAppSwiftUI
+//
+//  Created by Huseyin on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMapAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

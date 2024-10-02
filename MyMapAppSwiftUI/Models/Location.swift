cb755cb3cd87cf8c02abf81cd1c91989f46c53a1
Huseyin <benhuseyingundogdu@gmail.com>
2024-10-02 15:55:43 +0200

    Project setup with assets and Create Location Model and LocalDataService.

---
+++ MyMapAppSwiftUI/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  MyMapAppSwiftUI
+//
+//  Created by Huseyin on 02/10/2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
+
